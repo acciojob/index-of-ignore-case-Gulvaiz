f1cddb1878d788ae3334b60a7210234d85dfbc0e
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 02:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	  if(s1.indexOf(s2)){
+	 return(s1.indexOf(s2))
+  
+}
+	 else{
+	   return -1
+	 }
 }
 
 // Please do not change the code below
